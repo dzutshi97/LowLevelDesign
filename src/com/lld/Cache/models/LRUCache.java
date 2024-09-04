f66e878494f57ae61777d84ae0e4abb3a4feff85
dzutshi97 <103752774+dzutshi97@users.noreply.github.com>
2024-09-04 22:23:28 +0530

    Update LRUCache.java

--- src/com/lld/Cache/models/LRUCache.java
+++ src/com/lld/Cache/models/LRUCache.java
@@ -29,7 +29,6 @@ public class LRUCache<K,V> implements Cache<K,V>{
             }
         });
         t.start(); //imp
-
     }
 
     @Override
@@ -42,11 +41,11 @@ public class LRUCache<K,V> implements Cache<K,V>{
             deque.remove(key);
             deque.addFirst(key);
             CacheEntry<V> val =map.get(key);
+            val.setLastUpdatedTime(); //?
             return val.value;
         }finally {
             lock.readLock().unlock();
         }
-
     }
 
     @Override
@@ -70,7 +69,7 @@ public class LRUCache<K,V> implements Cache<K,V>{
     public void evict() {
         lock.writeLock().lock();
         try{
-            Iterator<K> iterator = deque.iterator(); //use iterator instead of for loop to avoid concurrent modification exception
+            Iterator<K> iterator = deque.iterator();
             while(iterator.hasNext()){
                 K key = iterator.next();
                 CacheEntry<V> entry = map.get(key);
@@ -86,3 +85,52 @@ public class LRUCache<K,V> implements Cache<K,V>{
         }
     }
 }
+/**
+ * public class Driver {
+ *     public static void main(String[] args) {
+ *         LRUCache<String, String> cache = new LRUCache<>(5000, 3);
+ *
+ *         // Adding entries to the cache
+ *         cache.put("1", "One");
+ *         cache.put("2", "Two");
+ *         cache.put("3", "Three");
+ *
+ *         System.out.println("Initial Cache:");
+ *         System.out.println("Key 1: " + cache.get("1")); // Should print One
+ *         System.out.println("Key 2: " + cache.get("2")); // Should print Two
+ *         System.out.println("Key 3: " + cache.get("3")); // Should print Three
+ *
+ *         // Adding one more entry, which should cause the first one to be evicted
+ *         cache.put("4", "Four");
+ *         System.out.println("\nAfter adding key 4:");
+ *         System.out.println("Key 1: " + cache.get("1")); // Should print null (evicted)
+ *         System.out.println("Key 2: " + cache.get("2")); // Should print Two
+ *         System.out.println("Key 3: " + cache.get("3")); // Should print Three
+ *         System.out.println("Key 4: " + cache.get("4")); // Should print Four
+ *
+ *         // Access key 2 to make it most recently used
+ *         cache.get("2");
+ *
+ *         // Adding another entry, which should cause key 3 to be evicted
+ *         cache.put("5", "Five");
+ *         System.out.println("\nAfter adding key 5:");
+ *         System.out.println("Key 2: " + cache.get("2")); // Should print Two
+ *         System.out.println("Key 3: " + cache.get("3")); // Should print null (evicted)
+ *         System.out.println("Key 4: " + cache.get("4")); // Should print Four
+ *         System.out.println("Key 5: " + cache.get("5")); // Should print Five
+ *
+ *         // Wait for the TTL to expire and check eviction
+ *         try {
+ *             Thread.sleep(6000);
+ *         } catch (InterruptedException e) {
+ *             e.printStackTrace();
+ *         }
+ *
+ *         System.out.println("\nAfter TTL expiration:");
+ *         System.out.println("Key 2: " + cache.get("2")); // Should print null (evicted due to TTL)
+ *         System.out.println("Key 4: " + cache.get("4")); // Should print null (evicted due to TTL)
+ *         System.out.println("Key 5: " + cache.get("5")); // Should print null (evicted due to TTL)
+ *     }
+ * }
+ *
+ */
