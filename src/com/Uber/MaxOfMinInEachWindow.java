2dc71895ef65370cf6a297604e8b8d6a3a35950a
dzutshi97 <103752774+dzutshi97@users.noreply.github.com>
2024-09-21 18:58:14 +0530

    Create MaxOfMinInEachWindow.java

---
+++ src/com/Uber/MaxOfMinInEachWindow.java
@@ -0,0 +1,14 @@
+package com.Uber;
+
+public class MaxOfMinInEachWindow {
+
+    //https://leetcode.com/problems/sliding-window-maximum/solutions/2059348/maximum-of-minimum-for-every-window-size/
+
+    /**
+     * int length = next[i] - prev[i] - 1;
+     *
+     *For each element arr[i],
+     * the length of the window in which arr[i] is the minimum can be determined by the distance between its
+     * next smaller element and previous smaller element.
+     */
+}
