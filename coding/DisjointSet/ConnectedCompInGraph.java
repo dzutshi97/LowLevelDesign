400c8c20b625d5c8e842e86fe451d8b34a69aab3
dzutshi97 <103752774+dzutshi97@users.noreply.github.com>
2024-09-21 11:04:47 +0530

    Update ConnectedCompInGraph.java

--- coding/DisjointSet/ConnectedCompInGraph.java
+++ coding/DisjointSet/ConnectedCompInGraph.java
@@ -63,5 +63,14 @@ class Solution{
          int res = solution.solve(n,edges);
          System.out.println(res);
      }
+     //Question link - https://neetcode.io/problems/count-connected-components
+     /**
+     There is an undirected graph with n nodes. There is also an edges array, where edges[i] = [a, b] means that there is an edge between node a and node b in the graph.
+
+    The nodes are numbered from 0 to n - 1.
+
+    Return the total number of connected components in that graph.
+     */
+     
 
 }
