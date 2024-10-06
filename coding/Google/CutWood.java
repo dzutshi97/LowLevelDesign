60c444d59b029eb9535125eb16591b955a29d055
dzutshi97 <103752774+dzutshi97@users.noreply.github.com>
2024-10-06 19:56:22 +0530

    Create CutWood.java

---
+++ coding/Google/CutWood.java
@@ -0,0 +1,6 @@
+package coding.Google;
+
+// https://leetcode.com/discuss/interview-question/354854/Facebook-or-Phone-Screen-or-Cut-Wood
+// Binary Search Question ~= Koko Eating Bananas
+public class CutWood {
+}
