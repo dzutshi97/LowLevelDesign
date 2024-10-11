4fbe738f651fd233e7abaeca9dc12ef4be7658b1
dzutshi97 <103752774+dzutshi97@users.noreply.github.com>
2024-10-11 12:13:41 +0530

    Create HighAcessEmployees.java

---
+++ coding/Atlassian/HighAcessEmployees.java
@@ -0,0 +1,5 @@
+package coding.Atlassian;
+
+public class HighAcessEmployees {
+    //Solved in LC - https://leetcode.com/problems/high-access-employees/submissions/1371439017/?envType=company&envId=atlassian&favoriteSlug=atlassian-thirty-days
+}
