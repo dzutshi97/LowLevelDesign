c12ac968cee22b186688e429065ca07915b0b1d4
dzutshi97 <103752774+dzutshi97@users.noreply.github.com>
2024-10-11 12:16:03 +0530

    Create MinNoOfStepsForKnight.java

---
+++ coding/BFS/MinNoOfStepsForKnight.java
@@ -0,0 +1,9 @@
+package coding.BFS;
+
+
+/**
+ * Minimum no of steps by knight to reach target - https://www.geeksforgeeks.org/minimum-steps-reach-target-knight/
+ *
+ */
+public class MinNoOfStepsForKnight {
+}
