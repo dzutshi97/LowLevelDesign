75249b43d1f10850d117a85627fc552ab7115080
dzutshi97 <103752774+dzutshi97@users.noreply.github.com>
2024-10-24 19:13:43 +0530

    Update Game.java

--- src/com/lld/TicTacToe/models/Game.java
+++ src/com/lld/TicTacToe/models/Game.java
@@ -17,63 +17,60 @@ public class Game {
 
     public boolean isWinner(Player currentPlayer){
         //logic to check for winner in row, col, diag & cross diagonal
-        boolean won= true;
+        boolean rowWin= true;
         //check in all rows
         for(int row = 0; row<board.board.length; row++) {
             for (int col = 0; col < board.board[0].length; col++) {
                 if (board.board[row][col] != currentPlayer.getSymbol()) {
-                    won = false;
+                    rowWin = false;
                     break;
                 }
             }
         }
+        if(rowWin){
+            isGameOver = true;
+            return true;
+        }
         //check in all cols
+        boolean colWin = true;
         for(int col=0; col<board.board[0].length; col++){
             for(int row=0; row<board.board.length; row++){
                 if(board.board[row][col] != currentPlayer.getSymbol()){
-                    won = false;
+                    colWin = false;
                     break;
                 }
             }
         }
+        if(colWin){
+            isGameOver = true;
+            return true;
+        }
         //check in daig
-        //check in cross-diag
-        for(int col=0; col<board.board[0].length; col++){
-            for(int row=0; row<board.board.length; row++){
-                if(row == col){
-                    if(board.board[row][col] != currentPlayer.getSymbol()){
-                        won = false;
-                        break;
-                    }
-                }
+        boolean diagWon = true;
+        for(int i=0; i<board.board.length; i++){
+            if(board.board[i][i] != currentPlayer.getSymbol()){
+                diagWon = false;
+                break;
             }
         }
-        if(won){
+        if(diagWon){
+            isGameOver = true;
+            return true;
+        }
+
+        //check in cross-diag
+        boolean crossDiagWon= true;
+        for(int i=0; i<board.board.length; i++){
+            if(board.board[i][board.board.length - i - 1] != currentPlayer.getSymbol()){
+                crossDiagWon = false;
+                break;
+            }
+        }
+        if(crossDiagWon){
             isGameOver = true;
             return true;
         }
         return false;
-        /** OR follow this approach of early exit by using 4 flags -
-         *    // Check the main diagonal
-         *     boolean diag1Win = true;
-         *     for (int i = 0; i < size; i++) {
-         *         if (board[i][i] != symbol) {
-         *             diag1Win = false;
-         *             break;
-         *         }
-         *     }
-         *     if (diag1Win) return true;
-         *
-         *     // Check the anti-diagonal (from top-right to bottom-left)
-         *     boolean diag2Win = true;
-         *     for (int i = 0; i < size; i++) {
-         *         if (board[i][size - i - 1] != symbol) { UNDERSTAND THIS
-         *             diag2Win = false;
-         *             break;
-         *         }
-         *     }
-         *     if (diag2Win) return true;
-         */
     }
 
     public void switchPlayer(){
@@ -86,16 +83,36 @@ public class Game {
 
     public void play(){
 
-        while(isGameOver){
+        while(!isGameOver){
             //current player will make a move
             MoveStrategy humanMoveStrategy = new HumanMoveStrategy();
-            currentPlayer.makeNextMove(humanMoveStrategy);
+            int[] pos = currentPlayer.makeNextMove(humanMoveStrategy);
+            int r = pos[0];
+            int c = pos[1];
+            board.board[r][c] = currentPlayer.getSymbol();
                     //check if he is the winner. Yes, then set game over
             if(isWinner(currentPlayer)){
+                return;
+            }
+
+            // Check if the board is full (resulting in a draw)
+            if (isBoardFull()) {
+                System.out.println("The game is a draw!");
+                isGameOver = true;
                 return;
             }
             //if not, switch the player
             switchPlayer();
         }
+    }
+    private boolean isBoardFull() {
+        for (int i = 0; i < board.board.length; i++) {
+            for (int j = 0; j < board.board[i].length; j++) {
+                if (board.board[i][j] == '-') {  // Assuming empty cells are represented by '-'
+                    return false;  // There is still space on the board
+                }
+            }
+        }
+        return true;  // No empty spaces left
     }
 }
