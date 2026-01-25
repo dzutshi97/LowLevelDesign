4a366f5127bf1f30b88b202b41491a4926919cef
dzutshi97 <103752774+dzutshi97@users.noreply.github.com>
2026-01-25 19:44:45 +0530

    Update MergeKSortedArrays.java

--- coding/PriorityQueue/MergeKSortedArrays.java
+++ coding/PriorityQueue/MergeKSortedArrays.java
@@ -1,63 +1,67 @@
 package coding.PriorityQueue;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
+class Solution {
+
+    class Element{
+        int value;
+        int idx;
+        int arrIdx;
+
+        public Element(int value, int idx, int arrIdx){
+            this.value= value;
+            this.idx=idx;
+            this.arrIdx= arrIdx;
+        }
+    }
+
+    public void mergeKSortedArrays(List<int[]> arrays) {
+
+        Queue<Element> pq = new PriorityQueue<>((e1, e2) -> e1.value - e2.value); //min heap - by default
+
+        for(int i=0;i<arrays.size(); i++){
+            int value = arrays.get(i)[0];
+            pq.add(new Element(value, 0, i));
+        }
+
+        List<Integer> ans = new ArrayList<Integer>();
+        while(!pq.isEmpty()){
+            Element curr = pq.remove();
+            ans.add(curr.value);
+            int currIdx = curr.idx;
+            int currArrIdx = curr.arrIdx;
+
+            if(currIdx + 1 < arrays.get(currArrIdx).length){
+                int[] currArr = arrays.get(currArrIdx);
+                int nextVal = currArr[currIdx + 1];
+//            OR    int nextVal = arrays.get(currArrIdx)[currIdx + 1];
+                pq.add(new Element(nextVal,currIdx + 1, currArrIdx));
+            }
+        }
+
+        for(int a: ans){
+            System.out.println(a+ " ");
+        }
+
+    }
+}
 public class MergeKSortedArrays {
 
     public static void main(String[] args) {
 
-        int[] a = new int[]{1,3};
-        int[] b = new int[]{2,4,6};
-        int[] c = new int[]{0, 9, 10, 11};
-        List<int[]> ipArrs = new ArrayList<>();
-        ipArrs.add(a);
-        ipArrs.add(b);
-        ipArrs.add(c);
-        Solution sol = new Solution();
-        int[] res = sol.solve(ipArrs);
-        for(int i=0; i<res.length; i++){
-            System.out.print(res[i] + " ");
-        }
+        int[] a = new int[]{1, 2, 3};
+        int[] b = new int[]{2, 3, 5, 7, 8};
+        int[] c = new int[]{6, 9, 11, 14};
+
+        List<int[]> al = new ArrayList<>();
+        al.add(a);
+        al.add(b);
+        al.add(c);
+
+        Solution s = new Solution();
+        s.mergeKSortedArrays(al);
     }
 }
 
-class Solution{
-    class Element{
-        int no;
-        int idx;
-        int arrIdx;
-
-        public Element(int no, int idx, int arrIdx) {
-            this.no = no;
-            this.idx = idx;
-            this.arrIdx = arrIdx;
-        }
-    }
-
-    PriorityQueue<Element> pq = new PriorityQueue<>((Element e, Element b) -> e.no - b.no); //min heap
-    public int[] solve(List<int[]> arrays){
-        int totalLength=0;
-
-        for(int i=0;i<arrays.size(); i++){
-            int[] a = arrays.get(i);
-            pq.offer(new Element(a[0],0,i));
-            totalLength += a.length;
-        }
-
-        int[] ans = new int[totalLength];
-        int k=0;
-
-        while(!pq.isEmpty()){
-            Element curr = pq.poll();
-            ans[k++] = curr.no;
-
-            if(curr.idx + 1 < arrays.get(curr.arrIdx).length){
-                pq.offer(new Element(arrays.get(curr.arrIdx)[curr.idx + 1],curr.idx+1, curr.arrIdx));
-            }
-        }
-        return ans;
-    }
-}
 //TC?
